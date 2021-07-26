9aad5265a165c67b77125d505f66f56e2dc408f5
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-26 16:09:34 -0300

    Delete jogo-da-memoria-reactjs directory

--- jogo-da-memoria-reactjs/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import MemoryGamer from './MemoryGamer';
-
-
-ReactDOM.render(<MemoryGamer />, document.getElementById('root'));
-
